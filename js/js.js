a4b76db623bc04d6bc6fbe37d08975b5db9d24d0
Nuno Martins <nunoormaker21@gmail.com>
2017-08-17 20:59:27 +0100

    Criação de Pastas e Arquivos Separados

---
+++ js/js.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Nuno Martins on 17-08-2017.
+ */
+$(document).ready(function($) {
+    var $apiKey = "Kljt3BmjypYsmedSl2Z3";
+    var $lower = 0;
+    var $higher = 0;
+
+    $.ajax({
+        url: "https://api.erepublik-deutschland.de/" + $apiKey + "/monetarymarket/gold",
+        type: "get",
+        datatype: 'json',
+        success: function(data) {
+            if (data.status == "ok") {
+                $.each(data.offers, function(index, offer) {
+                    /* This logs the offer object into your console */
+                    console.log("ID: " + index + " - Oferta: " + offer.offer.rate + " PTE");
+                    $("#t1").append("<tr><td>" + index + "</td><td>" + offer.seller.name + "</td><td>" + offer.offer.rate + " PTE</td></tr>");
+                    if ($lower != 0) {
+                        if (offer.offer.rate <= $lower) {
+                            $lower = offer.offer.rate;
+                        }
+                    }
+                    else {
+                        $lower = offer.offer.rate;
+                    }
+
+                    if ($higher != 0) {
+                        if (offer.offer.rate >= $higher) {
+                            $higher = offer.offer.rate;
+                        }
+                    }
+                    else {
+                        $higher = offer.offer.rate;
+                    }
+                });
+                $("#t2").append("<tr><td class='success'>" + $lower + "</td><td class='danger'>" + $higher + "</td></tr>");
+            } else {
+                console.log("An error occured: "+data.message);
+            }
+        },
+        error: function() {
+            console.log("An error occured");
+        }
+    });
+});
