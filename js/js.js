8419eae65c9d5875ca931fabbbca9ff4d698e823
Nuno Martins <nunoormaker21@gmail.com>
2018-02-09 22:45:43 +0000

    Adicionado sistema de refresh c/ notificação. Comentário de alguns códigos

--- js/js.js
+++ js/js.js
@@ -1,10 +1,54 @@
 /**
  * Created by Nuno Martins on 17-08-2017.
  */
+
+// Function to create cookies
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+// Function to get cookies
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+// Function to delete cookies
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
 $(document).ready(function($) {
     var $apiKey = "Kljt3BmjypYsmedSl2Z3";
+
+    // The lowest and highest prices
     var $lower = 0;
     var $higher = 0;
+
+    var refreshed = getCookie("Refresh");
+
+    // Check if the cookie "Refresh" exists => if the page refreshed
+    if (refreshed != null && refreshed != "") {
+        // If so, play the notification sound to alert the user that the page refreshed
+        $("#ringtone").trigger('play');
+        eraseCookie("Refresh");
+    }
+    else {
+        // If not, create a cookie
+        setCookie("Refresh", "true", 1);
+    }
 
     $.ajax({
         url: "https://api.erepublik-deutschland.de/" + $apiKey + "/monetarymarket/gold",
@@ -15,6 +59,7 @@ $(document).ready(function($) {
                 $.each(data.offers, function(index, offer) {
                     /* This logs the offer object into your console */
                     console.log("Data/Hora (eRepublik): " + offer.added + " - Oferta: " + offer.offer.rate + " PTE");
+                    // It creates a new row on the table with the information
                     $("#t1").append("<tr><td>" + offer.added + "</td><td><a href='https://www.erepublik.com/br/citizen/profile/" + offer.seller.id + "'>" + offer.seller.name + "</a></td><td>" + offer.offer.rate + " PTE</td></tr>");
                     if ($lower != 0) {
                         if (offer.offer.rate <= $lower) {
@@ -34,21 +79,15 @@ $(document).ready(function($) {
                         $higher = offer.offer.rate;
                     }
                 });
+
+                // On the 2nd table, append the lowest and the highest prices
                 $("#t2").append("<tr><td class='success'>" + $lower + "</td><td class='danger'>" + $higher + "</td></tr>");
             } else {
-                console.log("An error occured: "+data.message);
+                console.log("An error occured: " + data.message);
             }
         },
         error: function() {
             console.log("An error occured");
         }
     });
-    
-    var audio = {};
-    audio["sino"] = new Audio();
-    audio["sino"].src = "inserirSomAqui"
-    audio["sino"].addEventListener('load', function () {
-        audio["sino"].play();
-    });
-    
 });
